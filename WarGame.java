1a5f5d58f7422b6afa36a7d54f25ea0d1e532a2d
MananPatel-2708 <133824233+MananPatel-2708@users.noreply.github.com>
2023-06-14 12:45:34 -0400

    Update WarGame.java
    Added abstract game class and WarGame subclass

--- WarGame.java
+++ WarGame.java
@@ -76,3 +76,76 @@ class Player {
         return name;
     }
 }
+/**
+ * A class to represent the game
+ */
+abstract class Game {
+
+    private final String name;//the title of the game
+    private ArrayList<Player> players;// the players of the game
+
+    public Game(String name) {
+        this.name = name;
+        players = new ArrayList<>();
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the players of this game
+     */
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    /**
+     * @param players the players of this game
+     */
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    /**
+     * Play the game. This might be one method or many method calls depending on your game.
+     */
+    public abstract void play();
+
+    /**
+     * When the game is over, use this method to declare and display a winning player.
+     */
+    public abstract void declareWinner();
+}
+
+/**
+ * A class to represent the game of War
+ */
+class WarGame extends Game {
+    private final GroupOfCards deck;
+
+    public WarGame() {
+        super("War");
+        deck = new GroupOfCards();
+    }
+
+    @Override
+    public void play() {
+        // Create and shuffle the deck
+        createDeck();
+        deck.shuffle();
+
+        // Deal cards to players
+        dealCards();
+
+        // Start the game
+        while (atLeastTwoPlayersHaveCards()) {
+            playRound();
+        }
+
+        // Declare the winner
+        declareWinner();
+    }
