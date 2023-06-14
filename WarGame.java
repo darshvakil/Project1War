56991ead8c47947ba1e7e243883c576222701087
Vraj-20 <133669562+Vraj-20@users.noreply.github.com>
2023-06-14 12:39:40 -0400

    Updated WarGame.java
    added GroupOfCards and Players class

--- WarGame.java
+++ WarGame.java
@@ -18,3 +18,61 @@ class Card {
         return rank + " of " + suit;
     }
 }
+/**
+ * A class to represent a group of cards
+ */
+class GroupOfCards {
+    private ArrayList<Card> cards;
+
+    public GroupOfCards() {
+        cards = new ArrayList<>();
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card drawCard() {
+        if (cards.isEmpty()) {
+            return null;
+        }
+        return cards.remove(0);
+    }
+
+    public int getSize() {
+        return cards.size();
+    }
+}
+
+/**
+ * A class to represent a player
+ */
+class Player {
+    private final String name;
+    private final GroupOfCards hand;
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new GroupOfCards();
+    }
+
+    public void addCardToHand(Card card) {
+        hand.addCard(card);
+    }
+
+    public Card playCard() {
+        return hand.drawCard();
+    }
+
+    public boolean hasCards() {
+        return hand.getSize() > 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
